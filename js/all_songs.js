6bb6b0768b8af6b9e8a3b86ca390451865b5d421
iCarlycom <deangelorivera351@gmail.com>
2025-07-22 22:51:26 -0400

    Create all_songs.js

---
+++ js/all_songs.js
@@ -0,0 +1,122 @@
+var ALL_SONGS = {
+  16778: {
+    title: 'Freak the Freak Out',
+    artist: 'iSongs That Make iCarly Happy!',
+    album: 'Victorious',
+    length: 236.836958333333,
+    file_path: '\/assets\/1\/audio\/2010\/12\/06\/52115_2794011444.mp3',
+    album_image_url: '\/assets\/1\/image\/2010\/04\/23\/72091_1055972419.jpg'
+  },
+  15076: {
+    title: 'The Joke Is On You',
+    artist: 'iSongs That Make iCarly Happy!',
+    album: 'iSongs That Make iCarly Happy!',
+    length: 89.089625,
+    file_path: '\/assets\/1\/audio\/2010\/09\/24\/69303_1342704280.mp3',
+    album_image_url: '\/assets\/1\/image\/2009\/11\/06\/67970_3082332729.jpg'
+  },
+  11313: {
+    title: 'Finally Falling',
+    artist: 'Victoria Justice',
+    album: 'From the Show Victorious!',
+    length: 182.099775,
+    file_path: '\/assets\/1\/audio\/2010\/05\/14\/63354_2711813009.mp3',
+    album_image_url: '\/assets\/1\/image\/2010\/04\/23\/72091_1055972419.jpg'
+  },
+  10621: {
+    title: 'You\&#8217;re the Reason',
+    artist: 'Victoria Justice',
+    album: 'From the Show Victorious!',
+    length: 173.47955,
+    file_path: '\/assets\/1\/audio\/2010\/04\/23\/71070_2709043580.mp3',
+    album_image_url: '\/assets\/1\/image\/2010\/04\/23\/72091_1055972419.jpg'
+  },
+  9918: {
+    title: 'Make It Shine',
+    artist: 'Victoria Justice',
+    album: 'From the Show Victorious!',
+    length: 192.63075,
+    file_path: '\/assets\/1\/audio\/2010\/03\/06\/71013_4144230024.mp3',
+    album_image_url: '\/assets\/1\/image\/2010\/03\/09\/62205_3462932752.jpg'
+  },
+  7969: {
+    title: 'Carly\&#8217;s Happy Song',
+    artist: 'Soundtrack from Carly\&#8217;s Daydream',
+    album: 'Soundtrack from Carly\&#8217;s Daydreams',
+    length: 44.627,
+    file_path: '\/assets\/1\/audio\/2009\/11\/13\/61721_2302512599.mp3',
+    album_image_url: '\/assets\/1\/image\/2009\/11\/13\/65532_2945719125.jpg'
+  },
+  7694: {
+    title: 'Thunder',
+    artist: 'iSongs That Make iCarly Happy!',
+    album: 'iSongs That Make iCarly Happy!',
+    length: 231.018,
+    file_path: '\/assets\/1\/audio\/2009\/11\/05\/59183_896327135.mp3',
+    album_image_url: '\/assets\/1\/image\/2009\/11\/06\/68205_981543519.jpg'
+  },
+  7695: {
+    title: 'Freckles',
+    artist: 'iSongs That Make iCarly Happy!',
+    album: 'iSongs That Make iCarly Happy!',
+    length: 211.31475,
+    file_path: '\/assets\/1\/audio\/2009\/11\/05\/59335_1622183960.mp3',
+    album_image_url: '\/assets\/1\/image\/2009\/11\/06\/68014_807254466.jpg'
+  },
+  7696: {
+    title: 'Stay My Baby',
+    artist: 'iSongs That Make iCarly Happy!',
+    album: 'iSongs That Make iCarly Happy!',
+    length: 184.90475,
+    file_path: '\/assets\/1\/audio\/2009\/11\/05\/59502_1188929480.mp3',
+    album_image_url: '\/assets\/1\/image\/2009\/11\/06\/67970_3082332729.jpg'
+  },
+  7697: {
+    title: 'About You Now',
+    artist: 'iSongs That Make iCarly Happy!',
+    album: 'iSongs That Make iCarly Happy!',
+    length: 190.6676875,
+    file_path: '\/assets\/1\/audio\/2009\/11\/05\/59534_233291614.mp3',
+    album_image_url: '\/assets\/1\/image\/2009\/11\/06\/68301_3727785607.jpg'
+  },
+  7699: {
+    title: 'Whatever My Love',
+    artist: 'iSongs That Make iCarly Happy!',
+    album: 'iSongs That Make iCarly Happy!',
+    length: 94.8247916666667,
+    file_path: '\/assets\/1\/audio\/2009\/11\/05\/59780_3063380599.mp3',
+    album_image_url: '\/assets\/1\/image\/2009\/11\/06\/68238_3633378536.jpg'
+  },
+  7700: {
+    title: 'Dream',
+    artist: 'Harper',
+    album: 'Harper',
+    length: 127.08875,
+    file_path: '\/assets\/1\/audio\/2009\/11\/05\/59816_4247303973.mp3',
+    album_image_url: '\/assets\/1\/image\/2009\/11\/06\/75319_2165921950.jpg'
+  },
+  7702: {
+    title: 'Take Me Back',
+    artist: 'Backflesh',
+    album: 'Backflesh',
+    length: 101.1460625,
+    file_path: '\/assets\/1\/audio\/2009\/11\/05\/59968_482053834.mp3',
+    album_image_url: '\/assets\/1\/image\/2009\/11\/06\/67750_589882266.jpg'
+  },
+  7698: {
+    title: 'Headphones On',
+    artist: 'iSongs That Make iCarly Happy!',
+    album: 'iSongs That Make iCarly Happy!',
+    length: 182.7763125,
+    file_path: '\/assets\/1\/audio\/2009\/11\/05\/59683_3779374378.mp3',
+    album_image_url: '\/assets\/1\/image\/2009\/11\/06\/68273_4187088507.jpg'
+  },
+  7701: {
+    title: 'Leave It All To Me',
+    artist: 'iSongs That Make iCarly Happy!',
+    album: 'iSongs That Make iCarly Happy!',
+    length: 160.7795625,
+    file_path: '\/assets\/1\/audio\/2009\/11\/05\/59935_3180341016.mp3',
+    album_image_url: '\/assets\/1\/image\/2009\/11\/06\/53930_3722718371.jpg'
+  }
+}
