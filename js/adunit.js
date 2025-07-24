f2303c3a27ddd6e18723ab4956d871803f93829c
iCarlycom <deangelorivera351@gmail.com>
2025-07-24 00:18:48 -0400

    Create adunit.js

---
+++ js/adunit.js
@@ -0,0 +1,12 @@
+//Ad Unit DFP Update May 2 2014
+if (typeof MTVN == "undefined") MTVN = {};
+if (typeof MTVN.config == "undefined") MTVN.config = {};
+if (typeof MTVN.config.btg == "undefined") MTVN.config.btg = {};
+if (typeof MTVN.config.btg.DoubleClick == "undefined") MTVN.config.btg.DoubleClick = {};
+MTVN.config.btg.DoubleClick.auto = false;
+var vmn_page_data = {
+"metadata": { "property": "nick_web", "hub": "icarly" },
+"mappings": {
+"ad-unit": { "1": "@property", "2": "@hub" }
+}
+};
